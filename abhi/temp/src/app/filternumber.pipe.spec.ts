21a2d1d82fcd44002f0f9e2fb3830e2361a4068d
Abhilash <ad9956015@gmail.com>
2025-03-12 10:14:28 +0530

    Delete abhi directory

--- abhi/temp/src/app/filternumber.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilternumberPipe } from './filternumber.pipe';
-
-describe('FilternumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilternumberPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
